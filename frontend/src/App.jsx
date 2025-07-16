8c545e24fae6fc5104d55b9cf6bec85d4e078dd0
SeifElsayed <seifsayed121@gmail.com>
2025-07-17 01:58:08 +0300

    Refactor App component to conditionally render Navbar and Footer based on authentication page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,47 @@
 import { Toaster } from "react-hot-toast";
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import { Navbar } from './components/Navbar'
 import { Home } from './pages/Home'
 import { Footer } from "./components/Footer";
 import { Products } from "./pages/Products";
+import { Login } from "./pages/auth/Login";
+import { VerifyEmail } from "./pages/auth/VerifyEmail";
+import { ResetPassword } from "./pages/auth/ResetPassword";
 
 function App() {
+  const location = useLocation();
+  const isLoginPage = location.pathname.includes("/auth");
   return (
     <>
+      
       <Toaster
         position="top-center"
         reverseOrder={false}
       />
-      <Navbar />  
+      {!isLoginPage && <Navbar />}
       <Routes>
         <Route path='/' element={
             <Home />
         } /> 
+
         <Route path='/products' element={
           <Products />
         } />
+
+        <Route path="/auth" element={
+          <Login />
+        } />
+
+        <Route path="/auth/reset-password" element={
+          <ResetPassword />
+        } />
+
+        <Route path="/auth/verify-email" element={
+          <VerifyEmail />
+        } />
+
       </Routes>
-      <Footer />
+      {!isLoginPage && <Footer />}
     </>
   )
 }
