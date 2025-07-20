ad370b53905451a6380763d803a5ee3e414a1c4c
SeifElsayed <seifsayed121@gmail.com>
2025-07-20 17:58:47 +0300

    Add Sidebar component with navigation links and user context integration

--- frontend/src/components/admin/Sidebar.jsx
+++ frontend/src/components/admin/Sidebar.jsx
@@ -1,6 +1,6 @@
 import { useContext, useState } from 'react'
-import { UserContext } from '../context/UserContext'
-import { assets } from '../assets/assets';
+import { UserContext } from '../../context/UserContext'
+import { assets } from '../../assets/assets';
 import { Link } from 'react-router-dom';
 
 export const Sidebar = () => {
@@ -34,28 +34,28 @@ export const Sidebar = () => {
                         <p className='text-gray-100'>Dashboard</p>
                     </Link>
 
-                    <Link to="/add-product" className='flex items-center gap-4  hover:bg-[#1E88E5] p-2 rounded duration-300'>
+                    <Link to="/admin/add-product" className='flex items-center gap-4  hover:bg-[#1E88E5] p-2 rounded duration-300'>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5 text-white">
                             <path fillRule="evenodd" d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z" clipRule="evenodd" />
                         </svg>
                         <p className='text-gray-100'>Add Products</p>
                     </Link>
 
-                    <Link to="/list-products" className='flex items-center gap-4  hover:bg-[#1E88E5] p-2 rounded duration-300'>
+                    <Link to="/admin/list-products" className='flex items-center gap-4  hover:bg-[#1E88E5] p-2 rounded duration-300'>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5 text-white">
                             <path fillRule="evenodd" d="M2.625 6.75a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Zm4.875 0A.75.75 0 0 1 8.25 6h12a.75.75 0 0 1 0 1.5h-12a.75.75 0 0 1-.75-.75ZM2.625 12a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0ZM7.5 12a.75.75 0 0 1 .75-.75h12a.75.75 0 0 1 0 1.5h-12A.75.75 0 0 1 7.5 12Zm-4.875 5.25a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Zm4.875 0a.75.75 0 0 1 .75-.75h12a.75.75 0 0 1 0 1.5h-12a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
                         </svg>
                         <p className='text-gray-100'>List Products</p>
                     </Link>
 
-                    <Link to="/users" className='flex items-center gap-4  hover:bg-[#1E88E5] p-2 rounded duration-300'>
+                    <Link to="admin/users" className='flex items-center gap-4  hover:bg-[#1E88E5] p-2 rounded duration-300'>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5 text-white">
                             <path d="M4.5 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM14.25 8.625a3.375 3.375 0 1 1 6.75 0 3.375 3.375 0 0 1-6.75 0ZM1.5 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM17.25 19.128l-.001.144a2.25 2.25 0 0 1-.233.96 10.088 10.088 0 0 0 5.06-1.01.75.75 0 0 0 .42-.643 4.875 4.875 0 0 0-6.957-4.611 8.586 8.586 0 0 1 1.71 5.157v.003Z" />
                         </svg>
                         <p className='text-gray-100'>All Users</p>
                     </Link>
 
-                    <Link to="/all-orders" className=' flex items-center gap-4  hover:bg-[#1E88E5] p-2 rounded duration-300'>
+                    <Link to="admin/all-orders" className=' flex items-center gap-4  hover:bg-[#1E88E5] p-2 rounded duration-300'>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5 text-white">
                             <path fillRule="evenodd" d="M5.478 5.559A1.5 1.5 0 0 1 6.912 4.5H9A.75.75 0 0 0 9 3H6.912a3 3 0 0 0-2.868 2.118l-2.411 7.838a3 3 0 0 0-.133.882V18a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3v-4.162c0-.299-.045-.596-.133-.882l-2.412-7.838A3 3 0 0 0 17.088 3H15a.75.75 0 0 0 0 1.5h2.088a1.5 1.5 0 0 1 1.434 1.059l2.213 7.191H17.89a3 3 0 0 0-2.684 1.658l-.256.513a1.5 1.5 0 0 1-1.342.829h-3.218a1.5 1.5 0 0 1-1.342-.83l-.256-.512a3 3 0 0 0-2.684-1.658H3.265l2.213-7.191Z" clipRule="evenodd" />
                             <path fillRule="evenodd" d="M12 2.25a.75.75 0 0 1 .75.75v6.44l1.72-1.72a.75.75 0 1 1 1.06 1.06l-3 3a.75.75 0 0 1-1.06 0l-3-3a.75.75 0 0 1 1.06-1.06l1.72 1.72V3a.75.75 0 0 1 .75-.75Z" clipRule="evenodd" />
