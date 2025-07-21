03aca6365b8fe75da7143ffa7c3d128af70d7e38
SeifElsayed <seifsayed121@gmail.com>
2025-07-21 15:02:08 +0300

    Fix Sidebar component link paths and adjust positioning styles

--- frontend/src/components/admin/Sidebar.jsx
+++ frontend/src/components/admin/Sidebar.jsx
@@ -9,8 +9,7 @@ export const Sidebar = () => {
 
     return (
         <div>
-            <div className={` absolute md:relative w-1/2 md:w-[250px] md:top-0 md:left-0 border-gray-500 bg-[#263238] h-screen ${open ? 'top-0 left-0' : '-left-full'} transition-all duration-300`}>
-                
+            <div className={`  absolute md:relative w-1/2 md:w-[250px]  md:left-0 border-gray-500 bg-[#263238] h-screen ${open ? 'left-0' : '-left-full'} transition-all duration-300`}>
                 <div onClick={() => setOpen(!open)} 
                     className='w-fit p-2 rounded cursor-pointer bg-[#263238]  md:hidden'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10 border-1 rounded border-gray-500 p-1">
@@ -48,7 +47,7 @@ export const Sidebar = () => {
                         <p className='text-gray-100'>List Products</p>
                     </Link>
 
-                    <Link to="admin/users" className='flex items-center gap-4  hover:bg-[#1E88E5] p-2 rounded duration-300'>
+                    <Link to="/admin/users" className='flex items-center gap-4  hover:bg-[#1E88E5] p-2 rounded duration-300'>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5 text-white">
                             <path d="M4.5 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM14.25 8.625a3.375 3.375 0 1 1 6.75 0 3.375 3.375 0 0 1-6.75 0ZM1.5 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM17.25 19.128l-.001.144a2.25 2.25 0 0 1-.233.96 10.088 10.088 0 0 0 5.06-1.01.75.75 0 0 0 .42-.643 4.875 4.875 0 0 0-6.957-4.611 8.586 8.586 0 0 1 1.71 5.157v.003Z" />
                         </svg>
