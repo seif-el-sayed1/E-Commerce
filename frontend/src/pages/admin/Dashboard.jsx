8b234caffae8cabd0cfbc0a67b7ea4ec2323e586
SeifElsayed <seifsayed121@gmail.com>
2025-07-23 21:15:39 +0300

    create admin dashboard

--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -1,40 +1,77 @@
 import { useContext, useEffect } from 'react';
 import { UserContext } from '../../context/UserContext';
 import { ProductsContext } from '../../context/ProductsContext';
+import { OrderContext } from '../../context/OrderContext';
 
 export const Dashboard = () => {
     const { allUsers, getAllUsers } = useContext(UserContext);
-    const { getAllProducts, allProducts } = useContext(ProductsContext);
+    const { allProducts, getAllProducts } = useContext(ProductsContext);
+    const { allOrders, getAllOrders } = useContext(OrderContext);
 
     useEffect(() => {
         getAllUsers();
         getAllProducts();
+        getAllOrders();
     }, []);
-    
-    return (
-        <div className="m-5">
-            <h1 className="text-3xl font-bold text-[#1E88E5]">Dashboard</h1>
-            <div className='bg-[#263238] w-full rounded border-1 border-[#1E88E5] mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 '>
-                <div className='flex items-center gap-3 bg-[#121212] border-1 border-[#1E88E5] p-5 my-5 mx-2 rounded-lg'>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-                        <path d="M4.5 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM14.25 8.625a3.375 3.375 0 1 1 6.75 0 3.375 3.375 0 0 1-6.75 0ZM1.5 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM17.25 19.128l-.001.144a2.25 2.25 0 0 1-.233.96 10.088 10.088 0 0 0 5.06-1.01.75.75 0 0 0 .42-.643 4.875 4.875 0 0 0-6.957-4.611 8.586 8.586 0 0 1 1.71 5.157v.003Z" />
-                    </svg>
-                    <div>
-                        <h2 className='text-lg font-semibold text-[#1E88E5]'>Total Users :</h2>
-                        <p className="text-lg text-center font-bold">{allUsers.totalUsers || 0}</p>
-                    </div>
-                </div>
-                <div className='flex items-center gap-3 bg-[#121212] border-1 border-[#1E88E5] p-5 my-5 mx-2 rounded-lg'>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-                        <path d="M4.5 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM14.25 8.625a3.375 3.375 0 1 1 6.75 0 3.375 3.375 0 0 1-6.75 0ZM1.5 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM17.25 19.128l-.001.144a2.25 2.25 0 0 1-.233.96 10.088 10.088 0 0 0 5.06-1.01.75.75 0 0 0 .42-.643 4.875 4.875 0 0 0-6.957-4.611 8.586 8.586 0 0 1 1.71 5.157v.003Z" />
-                    </svg>
-                    <div>
-                        <h2 className='text-lg font-semibold text-[#1E88E5]'>Total products :</h2>
-                        <p className="text-lg text-center font-bold">{allProducts.totalProducts || 0}</p>
-                    </div>
-                </div>
 
+    return (
+        <div className="m-5 w-full">
+            <h1 className="text-3xl font-bold text-[#1E88E5]">Dashboard</h1>
+
+            <div className="bg-[#263238] w-full rounded border border-[#1E88E5] mt-5 flex flex-wrap justify-center gap-5 p-5">
+                <StatCard
+                    icon={
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" className="size-6">
+                            <path d="M4.5 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM14.25 8.625a3.375 3.375 0 1 1 6.75 0 3.375 3.375 0 0 1-6.75 0ZM1.5 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM17.25 19.128l-.001.144a2.25 2.25 0 0 1-.233.96 10.088 10.088 0 0 0 5.06-1.01.75.75 0 0 0 .42-.643 4.875 4.875 0 0 0-6.957-4.611 8.586 8.586 0 0 1 1.71 5.157v.003Z" />
+                        </svg>
+                    }
+                    label="Total Users"
+                    value={allUsers.totalUsers || 0}
+                />
+
+                <StatCard
+                    icon={
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" className="size-6">
+                            <path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
+                        </svg>
+                    }
+                    label="Total Products"
+                    value={allProducts.totalProducts || 0}
+                />
+
+                <StatCard
+                    icon={
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" className="size-5 text-white">
+                            <path fillRule="evenodd" d="M5.478 5.559A1.5 1.5 0 0 1 6.912 4.5H9A.75.75 0 0 0 9 3H6.912a3 3 0 0 0-2.868 2.118l-2.411 7.838a3 3 0 0 0-.133.882V18a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3v-4.162c0-.299-.045-.596-.133-.882l-2.412-7.838A3 3 0 0 0 17.088 3H15a.75.75 0 0 0 0 1.5h2.088a1.5 1.5 0 0 1 1.434 1.059l2.213 7.191H17.89a3 3 0 0 0-2.684 1.658l-.256.513a1.5 1.5 0 0 1-1.342.829h-3.218a1.5 1.5 0 0 1-1.342-.83l-.256-.512a3 3 0 0 0-2.684-1.658H3.265l2.213-7.191Z" clipRule="evenodd" />
+                            <path fillRule="evenodd" d="M12 2.25a.75.75 0 0 1 .75.75v6.44l1.72-1.72a.75.75 0 1 1 1.06 1.06l-3 3a.75.75 0 0 1-1.06 0l-3-3a.75.75 0 0 1 1.06-1.06l1.72 1.72V3a.75.75 0 0 1 .75-.75Z" clipRule="evenodd" />
+                        </svg>
+                    }
+                    label="Total Orders"
+                    value={allOrders.totalOrders || 0}
+                />
+
+                <StatCard
+                    icon={
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" className="size-6">
+                            <path d="M10.464 8.746c.227-.18.497-.311.786-.394v2.795a2.252 2.252 0 0 1-.786-.393c-.394-.313-.546-.681-.546-1.004 0-.323.152-.691.546-1.004ZM12.75 15.662v-2.824c.347.085.664.228.921.421.427.32.579.686.579.991 0 .305-.152.671-.579.991a2.534 2.534 0 0 1-.921.42Z" />
+                            <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v.816a3.836 3.836 0 0 0-1.72.756c-.712.566-1.112 1.35-1.112 2.178 0 .829.4 1.612 1.113 2.178.502.4 1.102.647 1.719.756v2.978a2.536 2.536 0 0 1-.921-.421l-.879-.66a.75.75 0 0 0-.9 1.2l.879.66c.533.4 1.169.645 1.821.75V18a.75.75 0 0 0 1.5 0v-.81a4.124 4.124 0 0 0 1.821-.749c.745-.559 1.179-1.344 1.179-2.191 0-.847-.434-1.632-1.179-2.191a4.122 4.122 0 0 0-1.821-.75V8.354c.29.082.559.213.786.393l.415.33a.75.75 0 0 0 .933-1.175l-.415-.33a3.836 3.836 0 0 0-1.719-.755V6Z" clipRule="evenodd" />
+                        </svg>
+                    }
+                    label="Total Profit"
+                    value={`$ ${allOrders.totalProfit || 0}`}
+                />
             </div>
         </div>
-    )
-}
+    );
+};
+
+// Component to reduce repetition
+const StatCard = ({ icon, label, value }) => (
+    <div className="flex items-center gap-3 bg-[#121212] border border-[#1E88E5] p-5 rounded-lg min-w-[250px]">
+        {icon}
+        <div>
+            <h2 className="text-lg font-semibold text-[#1E88E5]">{label} :</h2>
+            <p className="text-lg text-center font-bold">{value}</p>
+        </div>
+    </div>
+);
