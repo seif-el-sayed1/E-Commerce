3a7f060612ddf989d16645ea1ee78e534c56bb0c
SeifElsayed <seifsayed121@gmail.com>
2025-07-23 21:16:38 +0300

    add orders context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,7 @@ import App from './App.jsx'
 import { UserContextProvider } from "./context/UserContext.jsx";
 import { ProductsContextProvider } from "./context/ProductsContext.jsx";
 import { CartContextProvider } from "./context/CartContext.jsx";
+import {OrderContextProvider} from "./context/OrderContext.jsx";
 
 
 createRoot(document.getElementById('root')).render(
@@ -14,7 +15,9 @@ createRoot(document.getElementById('root')).render(
       <UserContextProvider>
         <ProductsContextProvider>
           <CartContextProvider>
-            <App />
+            <OrderContextProvider>
+              <App />
+            </OrderContextProvider>
           </CartContextProvider>
         </ProductsContextProvider>
       </UserContextProvider>
