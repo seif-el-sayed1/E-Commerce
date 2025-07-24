cf0f8764e216101cbe083d549f753be2c80a1726
SeifElsayed <seifsayed121@gmail.com>
2025-07-24 17:03:42 +0300

    add category , stock and sold

--- frontend/src/pages/ProductsDetails.jsx
+++ frontend/src/pages/ProductsDetails.jsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { ProductsContext } from '../context/ProductsContext';
 import { assets } from '../assets/assets';
 import { PopularProducts } from '../components/PopularProducts';
@@ -7,12 +7,19 @@ import { CartContext } from '../context/CartContext';
 
 export const ProductsDetails = () => {
     const { id } = useParams();
-    const { product, getProductById, loading } = useContext(ProductsContext);
+    const navigate = useNavigate();
+    const { product, getProductById, loading, setFilters, filters } = useContext(ProductsContext);
     const { addToCart, loading: cartLoading } = useContext(CartContext);
 
     useEffect(() => {
         getProductById(id);
     }, [id]);
+
+    const handleClick = (cat) =>{
+        setFilters({...filters, category: cat});
+        navigate("/products");
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    }
 
     if (loading) {
         return (
@@ -44,7 +51,16 @@ export const ProductsDetails = () => {
                             <span className="text-gray-300 ml-2 text-sm">4.0 Rating</span>
                         </div>
 
-                        <p className='text-gray-300 mb-6 leading-relaxed'>{product.description}</p>
+                        <p className='text-gray-300 mb-2 leading-relaxed'>{product.description}</p>
+                        <hr className='text-gray-600 mb-2' />
+
+                        <div className='flex flex-col md:flex-row md:gap-4'>
+                            <p className='text-gray-300 mb-2 text-sm'>Category: <span onClick={() => handleClick(product.category)}
+                                                                                        className='cursor-pointer underline pb-1 text-[#1E88E5] font-bold'>{product.category}</span></p>
+                            <p className='text-gray-300 mb-2 text-sm'>Stock: <span className='text-[#1E88E5] font-bold'>{product.stock}</span></p>
+                            <p className='text-gray-300 mb-2 text-sm'>Sold: <span className='text-[#1E88E5] font-bold'>{product.sold}</span></p>
+                        </div>
+                        <hr className='text-gray-600 mb-2' />
 
                         <div className='flex items-center gap-4 mb-6'>
                             <h3 className='text-2xl font-bold text-white'>${product.price && product.quantity ? product.finalPrice.toFixed(2) : product.price}</h3>
